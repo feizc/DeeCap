c5f06ca0d293b194bb59167254e32b414448472a
zcfei <3300949577@qq.com>
2021-10-03 16:34:47 +0800

    model change

--- dataset.py
+++ dataset.py
@@ -6,6 +6,11 @@ from torch.utils.data import Dataset
 from transformers import GPT2Tokenizer 
 
 
+
+SPECIAL_TOKENS = ['[BOS]', '[EOS]', '[SEP]', '[IMG]', '[TXT]', '[PAD]']
+SPECIAL_TOKENS_DICT = {'bos_token':'[BOS]', 'eos_token':'[EOS]', 'additional_special_tokens':['[IMG]', '[TXT]', '[SEP]'], 'pad_token':'[PAD]'}
+
+
 class SE2Dataset(Dataset): 
 
     def __init__(self, data_path, tokenizer):
@@ -18,19 +23,37 @@ class SE2Dataset(Dataset):
     def __getitem__(self, i):
         cap_dict = self.train_data[i]
         img_id = str(cap_dict['image_id']) + '_features' 
-        img = torch.FloatTensor(self.img_features[img_id])
+        img = self.img_features[img_id]
         txt = cap_dict['caption']
-        caption = self.tokenizer.tokenize(txt)
-        caption_id = self.tokenizer.convert_tokens_to_ids(caption) 
-        input_id = torch.Tensor(caption_id).long()
+        instance = build_input_from_segments(img, txt, self.tokenizer)
+        
+        input_ids = torch.Tensor(instance['input_ids']).long()
+        token_type_ids = torch.Tensor(instance['token_type_ids']).long() 
+        lm_labels = torch.Tensor(instance['lm_labels']).long()
+        img_features = torch.FloatTensor(img)
 
-        return img, input_id 
+        return img_features, input_ids, token_type_ids, lm_labels 
+
+def build_input_from_segments(img_features, caption, tokenizer): 
+    bos, eos, sep, img, txt = tokenizer.convert_tokens_to_ids(SPECIAL_TOKENS[:-1]) 
+    instance = {} 
+    caption_id = tokenizer.tokenize(caption)
+    caption_id = tokenizer.convert_tokens_to_ids(caption_id)
+    input_ids = [sep, bos] + caption_id
+    instance['input_ids'] = input_ids 
+    token_type_ids = [img] * len(img_features) + [txt]*len(input_ids)
+    instance['token_type_ids'] = token_type_ids 
+    lm_labels = [-1]*len(img_features) + [bos] + caption_id + [eos] 
+    instance['lm_labels'] = lm_labels 
+    return instance 
 
 
 if __name__ == "__main__": 
     data_path = 'data' 
-    tokenizer = GPT2Tokenizer.from_pretrained('model/origin_gpt', do_lower_case=True)
+    gpt_model_path = 'model/origin_gpt' 
+    tokenizer = GPT2Tokenizer.from_pretrained(gpt_model_path, do_lower_case=True) 
+    tokenizer.add_special_tokens(SPECIAL_TOKENS_DICT) 
     dataset = SE2Dataset(data_path, tokenizer) 
-    img_feature, txt = dataset[0] 
+    img_feature, txt, _, _ = dataset[0] 
     print(txt)
-    print(img_feature.size())
+    print(img_feature)
--- train.py
+++ train.py
@@ -1,6 +1,70 @@
 from transformers import * 
 import os 
+import torch 
 import json 
 import numpy as np 
 from model import SE2Model 
-from dataset import SE2Dataset
+from dataset import SE2Dataset 
+from torch.utils.data import DataLoader 
+
+
+SPECIAL_TOKENS = ['[BOS]', '[EOS]', '[SEP]', '[IMG]', '[TXT]', '[PAD]']
+SPECIAL_TOKENS_DICT = {'bos_token':'[BOS]', 'eos_token':'[EOS]', 'additional_special_tokens':['[IMG]', '[TXT]', '[SEP]'], 'pad_token':'[PAD]'}
+
+# parameters 
+train_dataset_path = 'data' 
+use_cuda = torch.cuda.is_available() 
+device = torch.device('cuda' if use_cuda else 'cpu') 
+gpt_model_path = 'model/origin_gpt' 
+ckpt_usage = False 
+
+lr = 6e-5 
+epochs = 1
+gradient_accumulation_steps = 1 
+print_freq = 1 
+
+
+def main(): 
+    if ckpt_usage == True: 
+        ckpt_path = '' 
+    else: 
+        tokenizer = GPT2Tokenizer.from_pretrained(gpt_model_path, do_lower_case=True) 
+        model = SE2Model.from_pretrained(gpt_model_path) 
+        tokenizer.add_special_tokens(SPECIAL_TOKENS_DICT) 
+        model.resize_token_embeddings(len(tokenizer)) 
+    model = model.to(device) 
+    optimizer = AdamW(model.parameters(), lr=lr) 
+
+    dataset = SE2Dataset(train_dataset_path, tokenizer) 
+    model = model.to(device) 
+    optimizer = AdamW(model.parameters(), lr=lr) 
+
+    train_dataset = SE2Dataset(train_dataset_path, tokenizer) 
+    # train_loader = DataLoader(train_dataset, batch_size=1) 
+
+    for epoch in range(epochs): 
+        train(model=model, tokenizer=tokenizer, optimizer=optimizer, dataset=train_dataset, epoch=epoch) 
+
+
+def train(model, tokenizer, optimizer, dataset, epoch): 
+    model.train() 
+
+    for instance in dataset: 
+        instance = tuple(input_tensor.to(device) for input_tensor in instance)
+        img_features, input_ids, token_type_ids, lm_labels = instance 
+        input_embs = model.transformer.wte(input_ids)
+        img_embs = model.img_ff(img_features)
+        print(input_embs.size())
+        print(img_embs.size())
+        print(img_features.size())
+        break 
+
+
+
+
+
+
+
+
+if __name__ == '__main__': 
+    main()
