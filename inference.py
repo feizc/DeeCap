f3b926714bdbf3646efd582dddbd8bec7b3c7df6
zcfei <3300949577@qq.com>
2021-10-08 23:04:14 +0800

    add model inference

---
+++ inference.py
@@ -0,0 +1,17 @@
+from transformers import * 
+import torch  
+import torch.nn.functional as F 
+import numpy as np 
+
+from model import SE2Model 
+from dataset import build_input_from_segments 
+
+
+
+
+
+
+
+if __name__ == '__main__': 
+    
+
